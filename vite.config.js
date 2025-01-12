6ce680b5ac77eff1aecbafcdab1a954e94af4cb3
Vinicius Brasil <vinicius.brasil@cartoriosdeprotesto.org.br>
2025-01-12 10:18:09 -0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     plugins: [
         vue(),
         laravel([
-            'resource/scss/app.scss',
+            //'resources/scss/app.scss',
+            'resources/css/app.css',
             'resources/js/app.js',
         ]),
     ]//, server: { host: '0.0.0.0', port: 5173, hmr: { host: '127.0.0.1', }, } 
@@ -18,36 +19,3 @@ export default defineConfig({
         }
     },
 });
-/*
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import path from 'path'
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '@': '/resources/js',            
-        },
-    },
-});
-*/
